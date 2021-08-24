6d00a1a2127f946c919ba129ccc87db071535e1b
casey <caseyjtite@gmail.com>
2021-08-24 03:34:21 +0000

    new finsta posts

--- app/actions.rb
+++ app/actions.rb
@@ -11,7 +11,7 @@
   end
   
   get '/' do
-    finstagram_post_shark = {
+    @finstagram_post_shark = {
       username: "sharky_j",
       avatar_url: "http://naserca.com/images/sharky_j.jpg",
       photo_url: "http://naserca.com/images/shark.jpg",
@@ -24,7 +24,7 @@
       }]
     }
   
-    finstagram_post_whale = {
+    @finstagram_post_whale = {
       username: "kirk_whalum",
       avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
       photo_url: "http://naserca.com/images/whale.jpg",
@@ -37,7 +37,7 @@
       }]
     }
   
-    finstagram_post_marlin = {
+    @finstagram_post_marlin = {
       username: "marlin_peppa",
       avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
       photo_url: "http://naserca.com/images/marlin.jpg",
@@ -49,4 +49,6 @@
         text: "lunchtime! ;)"
       }]
     }
+    @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+    erb(:index)
   end
